8c6997a9ad51ebe0a0017c77b38786110122d5b2
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-22 16:58:13 +0530

    program to Find Employee Attendance

---
+++ EmpAttendance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Day3
+{
+    class EmpAttendance
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 1;
+            int empRate = 50;
+            int empWage = 0;
+            int hours = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == fullTime)
+                hours = 8;
+            else
+                hours = 0;
+
+            empWage = empRate * hours;
+            Console.WriteLine("Employee Wage :" + empWage);
+
+        }
+    }
+}
