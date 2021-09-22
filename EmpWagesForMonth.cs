34d8cd8ea90d3e44ba48a7a39dfc10b093a7a157
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-22 17:01:49 +0530

    Find EmployeeWage For Month

---
+++ EmpWagesForMonth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+    class EmpWagesForMonth
+    {
+        static void Main(string[] args)
+        {
+            const int fullTime = 2;
+            const int partTime = 1;
+            const int empRate = 50;
+            const int totalDays = 20;
+            int totalWage = 0;
+            int empWage = 0;
+            int hours = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            for (int day = 0; day < totalDays; day++)
+            {
+                switch (empCheck)
+                {
+                    case fullTime:
+                        hours = 8;
+                        break;
+                    case partTime:
+                        hours = 4;
+                        break;
+                    default:
+                        hours = 0;
+                        break;
+                }
+
+            }
+        empWage = hours * empRate;
+        totalWage += empWage;
+            
+            Console.WriteLine("Employee Wage new:" + empWage);
+
+        }
+    }
+
