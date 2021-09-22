ab0d0e55f03296b5eae8eb7e76614f8967003c1e
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-22 16:59:58 +0530

    program to Find EmployeeWage for Parttime or Fulltime

---
+++ EmpTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Day4
+{
+    class EmpTime
+    {
+        static void Main(string[] args)
+        {
+            int fullTime = 2;
+            int partTime = 1;
+            int empRate = 50;
+
+            int empWage = 0;
+            int hours = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if (empCheck == fullTime)
+                hours = 8;
+            else if (empCheck == partTime)
+                hours = 4;
+            else
+                hours = 0;
+            empWage = empRate * hours;
+            Console.WriteLine("Employee Wage :" + empWage);
+
+        }
+    }
+}
